0e817007e6a80ce264d2f30708af3e3292990f3a
Nauras Jabari <nauras@agencyundone.com>
2021-10-07 17:11:30 -0700

    Feat: wired up data population for Index page and created baseline PageSection component

---
+++ components/PageSection.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="page-section">
+
+    {{ section.length }}
+
+  </div>
+</template>
+
+<script>
+// ====================================================================== Import
+// ====================================================================== Export
+export default {
+  name: 'PageSection',
+
+  props: {
+    section: {
+      type: Array,
+      required: true
+    }
+  }
+
+  // components: {
+  //   PageSection
+  // },
+
+  // async fetch ({ store }) {
+  //   await store.dispatch('global/getBaseData', 'general')
+  //   await store.dispatch('global/getBaseData', { key: 'index', data: IndexPageData })
+  // },
+
+  // computed: {
+  //   ...mapGetters({
+  //     siteContent: 'global/siteContent'
+  //   }),
+  //   seo () {
+  //     return this.$GetSeo(this.tag)
+  //   },
+  //   sections () {
+  //     return this.siteContent.index.page_content
+  //   }
+  // },
+
+  // mounted () {
+  //   console.log(this.sections)
+  // }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,51 @@
 <template>
   <div class="page page-index">
 
-    <nuxt />
+    <PageSection
+      v-for="(section, index) in sections"
+      :id="`section-${index + 1}`"
+      :key="index"
+      :section="section" />
 
   </div>
 </template>
 
 <script>
+// ====================================================================== Import
+import { mapGetters } from 'vuex'
+
+import IndexPageData from '@/content/pages/index.json'
+
+import PageSection from '@/components/PageSection'
+
 // ====================================================================== Export
 export default {
-  name: 'PageIndex'
+  name: 'PageIndex',
+
+  components: {
+    PageSection
+  },
+
+  async fetch ({ store }) {
+    await store.dispatch('global/getBaseData', 'general')
+    await store.dispatch('global/getBaseData', { key: 'index', data: IndexPageData })
+  },
+
+  computed: {
+    ...mapGetters({
+      siteContent: 'global/siteContent'
+    }),
+    seo () {
+      return this.$GetSeo(this.tag)
+    },
+    sections () {
+      return this.siteContent.index.page_content
+    }
+  },
+
+  mounted () {
+    console.log(this.sections)
+  }
 }
 </script>
 
