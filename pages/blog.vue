a703b50d7c5da023167dbfd79655cc78f1462ab9
svvimming <benmunozj@gmail.com>
2022-02-07 14:12:22 -0500

    chore Updated background layers array argument type + formatting

--- pages/blog.vue
+++ pages/blog.vue
@@ -30,7 +30,7 @@
 
       <BackgroundLayers
         id="page-blog-background-layers"
-        :layers-array="[3, 4, 5, 6]"
+        layers-array="3_4_5_6"
         :offset="pageBackgroundLayersOffset" />
 
     </div>
